c5b807284f27be5c16c812ec7b0ce1646deb9aef
andria660 <andrialezhava730@gmail.com>
2024-09-30 18:44:34 +0400

    Second Commit

--- level 83/classwork/c.c
+++ level 83/classwork/c.c
@@ -1,19 +1,19 @@
 
-// #include <stdio.h>
-// // dakomentebam rom imuSaos unda -nostdlib -lgcc gcc10-3.pdf -dan 228pg:244epg
-// int main(void)
-// {
+#include <stdio.h>
+// dakomentebam rom imuSaos unda -nostdlib -lgcc gcc10-3.pdf -dan 228pg:244epg
+int main(void)
+{
 
-//     // fprintf(stdout, "\nhello, world\n\n");
+    // fprintf(stdout, "\nhello, world\n\n");
     
-//     printf("\nprivet, world\n\n");
+    printf("\nprivet, world\n\n");
     
     
-// //     int r = 0;
-// //     if((r = printf("\nhello, world\n")) < 0)
-// //      perror("printf error");
-// //     else
-// //         fprintf(stdout, "amount of symbols %d\n\n", r);
-//     return 0;
-// }
+//     int r = 0;
+//     if((r = printf("\nhello, world\n")) < 0)
+//      perror("printf error");
+//     else
+//         fprintf(stdout, "amount of symbols %d\n\n", r);
+    return 0;
+}
 
